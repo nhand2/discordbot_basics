0445eabcd130a82ad7d4e89979ae97901de75531
grrrdn <derek.nhan@gmail.com>
2017-09-24 17:51:39 -0700

    Added basic discord bot solution

---
+++ DiscordBot/src/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using DSharpPlus;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    class Program
+    {
+        static DiscordClient discord;
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "MzYxNjY2OTUxODk2MTA0OTYy.DKncLA.pu6WUjDFSFJFI7331ThYXu29GkM",
+                TokenType = TokenType.Bot
+            });
+
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
