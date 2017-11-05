42e6d94d89a14867312fc7562f68784a0791c639
grrrdn <derek.nhan@gmail.com>
2017-11-05 01:18:09 -0700

    Removed token.

--- DiscordBot/src/Program.cs
+++ DiscordBot/src/Program.cs
@@ -1,39 +1,54 @@
 ﻿using System;
+using System.IO;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+using DSharpPlus.VoiceNext;
 using System.Threading.Tasks;
 
 namespace DiscordBot
 {
     class Program
     {
-        static DiscordClient discord;
-        static CommandsNextModule commands;
+        #region Private
+        private static DiscordClient discord;
+        private static CommandsNextModule commands;
+        private static InteractivityModule interactivity;
+        private static VoiceNextClient voice;
+        #endregion
 
-        static void Main(string[] args)
+        #region Public
+        public static string OutputFolder = $"{Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar}videos";
+        #endregion
+
+        static void Main (string[] args)
         {
             MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
         }
 
-        static async Task MainAsync(string[] args)
+        static async Task MainAsync (string[] args)
         {
             discord = new DiscordClient(new DiscordConfiguration
             {
-                Token = "MzYxNjY2OTUxODk2MTA0OTYy.DKncLA.pu6WUjDFSFJFI7331ThYXu29GkM",
+                Token = "YOUR_KEY_HERE",
                 TokenType = TokenType.Bot,
                 UseInternalLogHandler = true,
                 LogLevel = LogLevel.Debug
             });
 
-            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+            interactivity = discord.UseInteractivity();
+
+            voice = discord.UseVoiceNext ();
+
+            commands = discord.UseCommandsNext (new CommandsNextConfiguration
             {
                 StringPrefix = "g."
             });
 
-            commands.RegisterCommands<Commands>();
+            commands.RegisterCommands<Commands> ();
 
-            await discord.ConnectAsync();
-            await Task.Delay(-1);
+            await discord.ConnectAsync ();
+            await Task.Delay (-1);
         }
     }
 }
